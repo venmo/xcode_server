c094e879326e27dd4fa58c090eaaabd8c4f8903b
Mickey Reiss <mickey.reiss@getbraintree.com>
2015-06-03 15:00:38 -0700

    Add a sloppy implementation of create_bot

--- lib/xcode_server/server.rb
+++ lib/xcode_server/server.rb
@@ -1,4 +1,5 @@
 require 'xcode_server/server/networking'
+require 'SecureRandom'
 
 module XcodeServer
   class Server
@@ -19,6 +20,68 @@ module XcodeServer
     def destroy_bot(id, rev = nil)
       rev ||= get_json("bots/#{id}")['_rev']
       delete("bots/#{id}/#{rev}").code.to_i == 204
+    end
+
+    def create_bot(performs_test_action: true,
+                   performs_archive_action: false,
+                   performs_analyze_action: true,
+                   scheme_name: nil,
+                   branch_name: "master",
+                   repo_identifier: nil,
+                   working_copy_path: nil,
+                   project_path: nil,
+                   project_url: nil)
+      res = post('bots',
+        group: {
+          name: SecureRandom.uuid
+        },
+        configuration: {
+          builtFromClean: 1, # Always
+          periodicScheduleInterval: 0, # Run manually
+          performs_test_action: performs_test_action,
+          performs_analyze_action: performs_analyze_action,
+          scheme_name: scheme_name,
+          sourceControlBlueprint: {
+            DVTSourceControlWorkspaceBlueprintLocationsKey: {
+              repo_identifier => {
+                DVTSourceControlBranchIdentifierKey: branch_name,
+                DVTSourceControlBranchOptionsKey: 156, # Mystery meat
+                DVTSourceControlWorkspaceBlueprintLocationTypeKey: "DVTSourceControlBranch" # "Pull a git branch"
+              }
+            },
+            DVTSourceControlWorkspaceBlueprintPrimaryRemoteRepositoryKey: repo_identifier,
+            DVTSourceControlWorkspaceBlueprintRemoteRepositoryAuthenticationStrategiesKey: {
+              repo_identifier => {
+                DVTSourceControlWorkspaceBlueprintRemoteRepositoryAuthenticationTypeKey: "DVTSourceControlAuthenticationStrategy"
+              }
+            },
+            DVTSourceControlWorkspaceBlueprintWorkingCopyStatesKey: {
+              repo_identifier => 0
+            },
+            DVTSourceControlWorkspaceBlueprintIdentifierKey: repo_identifier,
+            DVTSourceControlWorkspaceBlueprintWorkingCopyPathsKey: {
+              repo_identifier => working_copy_path
+            },
+            DVTSourceControlWorkspaceBlueprintNameKey: project_path,
+            DVTSourceControlWorkspaceBlueprintVersion: 203, # Mystery meat
+            DVTSourceControlWorkspaceBlueprintRelativePathToProjectKey: project_path,
+            DVTSourceControlWorkspaceBlueprintRemoteRepositoriesKey: [{
+              DVTSourceControlWorkspaceBlueprintRemoteRepositoryURLKey: project_url,
+              DVTSourceControlWorkspaceBlueprintRemoteRepositorySystemKey: "com.apple.dt.Xcode.sourcecontrol.Git",
+              DVTSourceControlWorkspaceBlueprintRemoteRepositoryIdentifierKey: repo_identifier
+            }]
+          },
+          hourOfIntegration: 0,
+          scheduleType: 2, # "On Commit" schedule
+          performs_archive_action: performs_archive_action,
+          testingDestinationType: 0 # "iOS All Devices and All Simulators"
+        },
+        requiresUpgrade: false, # Mystery meat
+        name: "#{working_copy_path} - #{scheme_name} @ #{branch_name}",
+        type: 1 # Mystery meat
+      )
+
+      Bot.new(self, JSON.load(res.body))
     end
   end
 end
--- lib/xcode_server/server/networking.rb
+++ lib/xcode_server/server/networking.rb
@@ -1,25 +1,35 @@
 require 'net/http'
 require 'json'
+require 'openssl'
 
 module XcodeServer
   class Server
     module Networking
 
-      def get(path)
-        path = "/xcode/api/#{path}"
-        http.request(Net::HTTP::Get.new(path))
+      def get(endpoint)
+        http.request(Net::HTTP::Get.new(path_to(endpoint)))
       end
 
-      def get_json(path)
-        JSON.load(get(path).body)
+      def post(endpoint, params)
+        puts params.to_json
+        req = Net::HTTP::Post.new(path_to(endpoint))
+        req['Content-Type'] = 'application/json'
+        http.request(req, params.to_json)
       end
 
-      def delete(path)
-        path = "/xcode/api/#{path}"
-        http.request(Net::HTTP::Delete.new(path))
+      def get_json(endpoint)
+        JSON.load(get(endpoint).body)
+      end
+
+      def delete(endpoint)
+        http.request(Net::HTTP::Delete.new(path_to(endpoint)))
       end
 
       private
+
+      def path_to(endpoint)
+        "/xcode/api/#{endpoint}"
+      end
 
       def http
         @_http ||= begin
